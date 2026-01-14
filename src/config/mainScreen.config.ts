aa10daaf265c4793f24e948f23ce52b09e698d6e
kirill <kirill.korev.mev@gmail.com>
2026-01-14 11:09:47 +0300

    правки

--- src/config/mainScreen.config.ts
+++ src/config/mainScreen.config.ts
@@ -1,10 +1,10 @@
 import img01 from '@img/mainScreen/01.jpg';
-import img02 from '@img/mainScreen/02.jpg';
-import img03 from '@img/mainScreen/03.jpg';
+import img02 from '@img/mainScreen/02.webp';
+import img03 from '@img/mainScreen/03.webp';
 import img04 from '@img/mainScreen/04.jpg';
 import img01Mob from '@img/mainScreen/01_mob.png';
-import img02Mob from '@img/mainScreen/02_mob.jpg';
-import img03Mob from '@img/mainScreen/03_mob.jpg';
+import img02Mob from '@img/mainScreen/02_mob.png';
+import img03Mob from '@img/mainScreen/03_mob.png';
 import img04Mob from '@img/mainScreen/04_mob.jpg';
 
 const mainScreenConfig = [
@@ -13,7 +13,16 @@ const mainScreenConfig = [
         img: img01,
         imgMob: img01Mob,
     },
-
+    {
+        id: 'ms02',
+        img: img02,
+        imgMob: img02Mob,
+    },
+    {
+        id: 'ms03',
+        img: img03,
+        imgMob: img03Mob,
+    },
     {
         id: 'ms04',
         img: img04,
